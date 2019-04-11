4d47dad1cc3e544edeee76da545efec82be6b7a2
tjr226 <tjr226@gmail.com>
2019-04-11 13:42:00 -0400

    tried some animations for menu buton dropdown, no success yet

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -18,6 +18,9 @@ class Dropdown {
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
+
+    // this.content.classList.toggle('updown');
+    // this.content.classList.toggle('target');
   }
 }
 
